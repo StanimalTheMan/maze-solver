938265c3628782bb2d6c0141af708b3eb0310867
StanimalTheMan <sjc591@nyu.edu>
2023-07-26 23:56:45 -0400

    Make change from root to canvas

--- graphics.py
+++ graphics.py
@@ -3,12 +3,11 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self._root = Tk()
-        self._root.protocol("WM_DELETE_WINDOW", self.close)
         self._root.title("Python Maze Solver App")
-        self._root.configure(width=width, height=height)
-        self._canvas = Canvas()
-        self._canvas.pack()
+        self._canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self._canvas.pack(fill=BOTH, expand=1)
         self._is_running = False
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
 
 
     def redraw(self):
