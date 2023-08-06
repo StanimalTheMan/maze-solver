da41a2d9f2c1666ea8fe55c088d2bf8cf6a6cc57
StanimalTheMan <sjc591@nyu.edu>
2023-08-06 09:11:53 -0400

    Draw some lines by making point and line class

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(0, 0), Point(20, 20)), "red")
+    win.draw_line(Line(Point(0, 0), Point(500, 20)), "black")
     win.wait_for_close()
 
 if __name__ == '__main__':
