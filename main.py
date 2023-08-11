bf4be6c0342ada7a01fefb366341133eb975c7b2
StanimalTheMan <sjc591@nyu.edu>
2023-08-11 10:59:20 -0400

    Animate via list of column cells not rows

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from cell import Cell
 from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    maze = Maze(100, 100, 10, 10, 100, 100, win)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     # c1 = Cell(win)
     # c1.has_right_wall = False
--- maze.py
+++ maze.py
@@ -12,6 +12,7 @@ class Maze:
         cell_size_y,
         win,
     ):
+        self._cells = []
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -19,25 +20,42 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+
         self._create_cells()
 
     def _create_cells(self):
-        self._cells = []
-        for row in range(self._num_rows):
-            # list of cell objects
-            self._cells = [Cell(self._win) for _ in range(self._num_cols)]
+        # self._cells = []
+        # for row in range(self._num_rows):
+        #     # list of cell objects
+        #     self._cells = [Cell(self._win) for _ in range(self._num_cols)]
 
-        #  draw each Cell
-        for row in range(self._num_rows):
-            for col in range(self._num_cols):
-                self._draw_cell(row, col)
+        # #  draw each Cell
+        # for row in range(self._num_rows):
+        #     for col in range(self._num_cols):
+        #         self._draw_cell(row, col)
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
                 
     def _draw_cell(self, i, j):
-        x1 = self._x1 + j * self._cell_size_x
-        y1 = self._y1 + i * self._cell_size_y
-        x2 = self._x1 + (j + 1) * self._cell_size_x
-        y2 = self._y1 + (i + 1) * self._cell_size_y
-        self._cells[i].draw(x1, y1, x2, y2)
+        # x1 = self._x1 + j * self._cell_size_x
+        # y1 = self._y1 + i * self._cell_size_y
+        # x2 = self._x1 + (j + 1) * self._cell_size_x
+        # y2 = self._y1 + (i + 1) * self._cell_size_y
+        # self._cells[i].draw(x1, y1, x2, y2)
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
         self._animate()
 
     def _animate(self):
