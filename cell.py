bbe9aaa41beab240ead1a167721bc3af2a920e38
StanimalTheMan <sjc591@nyu.edu>
2023-08-11 09:27:27 -0400

    First pass at drawing a path between 2 cells

--- cell.py
+++ cell.py
@@ -12,7 +12,7 @@ class Cell:
         self._y2 = None
         self._win = win
 
-    def draw(self, x1, y1, x2, y2):
+    def draw(self, x1, y1, x2, y2, fill_color="black"):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -25,3 +25,16 @@ class Cell:
             self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+
+    def draw_move(self, to_cell, undo=False):
+        from_cell_center_x = (self._x1 + self._x2) // 2
+        from_cell_center_y = (self._y1 + self._y2) // 2
+        to_cell_center_x = (to_cell._x1 + to_cell._x2) // 2
+        to_cell_center_y = (to_cell._y1 + to_cell._y2) // 2
+        if undo:
+            self._win.draw_line(Line(Point(from_cell_center_x, from_cell_center_y),
+                                Point(to_cell_center_x, to_cell_center_y)), "gray")
+        else:
+            self._win.draw_line(Line(Point(from_cell_center_x, from_cell_center_y),
+                                Point(to_cell_center_x, to_cell_center_y)), "red")
+                
